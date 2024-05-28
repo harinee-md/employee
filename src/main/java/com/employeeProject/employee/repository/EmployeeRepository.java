d582304b32fb935fd59a2c0563e871804d8802a2
HARINEE M D <727721eucb017@skcet.ac.in>
2024-05-28 13:23:37 +0530

    Add files via upload

---
+++ src/main/java/com/employeeProject/employee/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.employeeProject.employee.repository;
+
+
+import com.employeeProject.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
